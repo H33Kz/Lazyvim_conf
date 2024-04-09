88a3db72cdc9384ff3a4825ae328ee18907e2f63
H33Kz <qlmejk@gmail.com>
2024-04-09 19:46:31 +0200

    Update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options hereby
 
 vim.opt.relativenumber = false
+
+if vim.fn.has("gui_running") then
+  -- vim.cmd("set guifont=JetBrains Mono:h13")
+  vim.opt.guifont = "JetBrains Mono:h13"
+end
